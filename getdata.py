57f9b2fad4634f367107ba609c0ddc72c5acbcd3
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-03 12:13:29 +0200

    feat: access apple data

--- getdata.py
+++ getdata.py
@@ -4,17 +4,15 @@ from configparser import ConfigParser
 
 config = ConfigParser()
 config.read('config.cfg')
-
 api_key = config['API']['api_key']
-print(api_key)
+
 # Define the URL of the API endpoint you want to request
-url = 'https://api.example.com/data'
+url = 'https://financialmodelingprep.com/api/v3/historical-price-full/AAPL'
 
 
 def main():
     # Make a GET request to the API endpoint
-    """response = requests.get(url)
-
+    response = requests.get(url+'?apikey='+api_key)
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         # Extract the JSON data from the response
@@ -23,44 +21,8 @@ def main():
         print(data)
     else:
         # If the request was not successful, print an error message
-        print(f"Error: {response.status_code}")"""
+        print(f"Error: {response.status_code}")
     
-    # JSON data
-    data = {
-        "symbol": "AAPL",
-        "historical": [
-            {
-                "date": "2023-10-06",
-                "open": 173.8,
-                "high": 176.61,
-                "low": 173.18,
-                "close": 176.53,
-                "adjClose": 176.53,
-                "volume": 21712747,
-                "unadjustedVolume": 21712747,
-                "change": 2.73,
-                "changePercent": 1.57077,
-                "vwap": 175.44,
-                "label": "October 06, 23",
-                "changeOverTime": 0.0157077
-            },
-            {
-                "date": "2023-10-05",
-                "open": 173.79,
-                "high": 175.45,
-                "low": 172.68,
-                "close": 174.91,
-                "adjClose": 174.91,
-                "volume": 48251046,
-                "unadjustedVolume": 47369743,
-                "change": 1.12,
-                "changePercent": 0.64446,
-                "vwap": 174.23,
-                "label": "October 05, 23",
-                "changeOverTime": 0.0064446
-            }
-        ]
-    }
     # Convert JSON data to pandas DataFrame
     df_apple = pd.json_normalize(data['historical'])
 

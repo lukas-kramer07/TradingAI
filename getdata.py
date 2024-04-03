545e6654982d464801c3e37d16d83cf05017fe0a
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-03 11:43:46 +0200

    feat: add config to store api_key

--- getdata.py
+++ getdata.py
@@ -1,47 +1,35 @@
 import pandas as pd
+import requests
+from configparser import ConfigParser
 
-# JSON data sample
-json_data = {
-    "symbol": "AAPL",
-    "historical": [
-        {
-            "date": "2023-10-06",
-            "open": 173.8,
-            "high": 176.61,
-            "low": 173.18,
-            "close": 176.53,
-            "adjClose": 176.53,
-            "volume": 21712747,
-            "unadjustedVolume": 21712747,
-            "change": 2.73,
-            "changePercent": 1.57077,
-            "vwap": 175.44,
-            "label": "October 06, 23",
-            "changeOverTime": 0.0157077
-        },
-        {
-            "date": "2023-10-05",
-            "open": 173.79,
-            "high": 175.45,
-            "low": 172.68,
-            "close": 174.91,
-            "adjClose": 174.91,
-            "volume": 48251046,
-            "unadjustedVolume": 47369743,
-            "change": 1.12,
-            "changePercent": 0.64446,
-            "vwap": 174.23,
-            "label": "October 05, 23",
-            "changeOverTime": 0.0064446
-        }
-    ]
-}
+config = ConfigParser()
+config.read('config.cfg')
 
-# Convert JSON data to pandas DataFrame
-df_apple = pd.json_normalize(json_data['historical'])
+my_key = config['whatever']['key']
 
-# Display the DataFrame
-print(df_apple)
+# Define the URL of the API endpoint you want to request
+url = 'https://api.example.com/data'
 
-# Save data in data folder
-df_apple.to_pickle('data/apple')
+
+def main():
+    # Make a GET request to the API endpoint
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Extract the JSON data from the response
+        data = response.json()
+        # Process the data as needed
+        print(data)
+    else:
+        # If the request was not successful, print an error message
+        print(f"Error: {response.status_code}")
+
+    # Convert JSON data to pandas DataFrame
+    df_apple = pd.json_normalize(data['historical'])
+
+    # Display the DataFrame
+    print(df_apple)
+
+    # Save data in data folder
+    df_apple.to_pickle('data/apple')

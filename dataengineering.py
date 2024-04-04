57646ac8f404eedffc7257578091cc5b64021a15
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-04 11:18:50 +0200

    feat: reorganize code

--- dataengineering.py
+++ dataengineering.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_pickle('data/apple')
-print(df.tail())
+FILENAME = 'data/apple'
 
-plot_cols = ['open', 'change', 'volume']
-date_time = pd.to_datetime(df.pop('date'), format='%Y-%m-%d')
-plot_features = df[plot_cols]
-plot_features.index = date_time
-_ = plot_features.plot(subplots=True)
+def plot(df):
+    plot_cols = ['open', 'change', 'volume']
+    date_time = pd.to_datetime(df.pop('date'), format='%Y-%m-%d')
+    plot_features = df[plot_cols]
+    plot_features.index = date_time
+    _ = plot_features.plot(subplots=True)
 
-plot_features = df[plot_cols][:20]
-plot_features.index = date_time[:20]
-_ = plot_features.plot(subplots=True)
-plt.show()
+    plot_features = df[plot_cols][:20]
+    plot_features.index = date_time[:20]
+    _ = plot_features.plot(subplots=True)
+    plt.show()
+
+def main():
+    df = pd.read_pickle(FILENAME)
+    plot(df)
+
+if __name__ == '__main__':
+    main()

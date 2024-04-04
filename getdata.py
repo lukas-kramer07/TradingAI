ba67d84fa55988e1f32929668a4ff0b018b67497
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-04 11:25:15 +0200

    feat: add getdata function

--- getdata.py
+++ getdata.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 from configparser import ConfigParser
+from datetime import datetime
 
 config = ConfigParser()
 config.read('config.cfg')
@@ -9,8 +10,7 @@ api_key = config['API']['api_key']
 # Define the URL of the API endpoint you want to request
 url = 'https://financialmodelingprep.com/api/v3/historical-price-full/AAPL'
 
-
-def main():
+def getdata():
     # Make a GET request to the API endpoint
     response = requests.get(url+'?apikey='+api_key)
     # Check if the request was successful (status code 200)
@@ -21,15 +21,19 @@ def main():
     else:
         # If the request was not successful, print an error message
         print(f"Error: {response.status_code}")
-    
+        
     # Convert JSON data to pandas DataFrame
-    df_apple = pd.json_normalize(data['historical'])
+    df = pd.json_normalize(data['historical'])
+    return df
 
+def main():
+    
+    df = getdata()
     # Display the DataFrame
-    print(df_apple)
+    print(df)
     
     # Save data in data folder
-    df_apple.to_pickle('data/apple')
+    df.to_pickle('data/apple')
 
 if __name__ == '__main__':
     main()

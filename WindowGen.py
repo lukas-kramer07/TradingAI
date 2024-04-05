9e8e27d34bbd6cd6cff3442b2d755644b9865823
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-05 11:43:22 +0200

    feat: add make_dataset method

--- WindowGen.py
+++ WindowGen.py
@@ -90,4 +90,17 @@ class WindowGenerator():
 
     plt.xlabel('Time [h]')
 
+def make_dataset(self, data):
+    data = np.array(data, dtype=np.float32)
+    ds = tf.keras.utils.timeseries_dataset_from_array(
+        data=data,
+        targets=None,
+        sequence_length=self.total_window_size,
+        sequence_stride=1,
+        shuffle=True,
+        batch_size=32,)
+
+    ds = ds.map(self.split_window)
+
+    return ds
     

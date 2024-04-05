8ef95a394dd84c64f77fd61c8c10d0fdc71f644a
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-05 11:40:59 +0200

    feat: add split_window method

--- WindowGen.py
+++ WindowGen.py
@@ -41,3 +41,17 @@ class WindowGenerator():
         f'Input indices: {self.input_indices}',
         f'Label indices: {self.label_indices}',
         f'Label column name(s): {self.label_columns}'])
+  def split_window(self, features):
+    inputs = features[:, self.input_slice, :]
+    labels = features[:, self.labels_slice, :]
+    if self.label_columns is not None:
+        labels = tf.stack(
+            [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+            axis=-1) #only output the features that are required by the labels
+
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+    inputs.set_shape([None, self.input_width, None])
+    labels.set_shape([None, self.label_width, None])
+
+    return inputs, labels

2b17770ecfa625a7ff6e852ad9793b97ee087b8d
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-05 11:55:56 +0200

    feat: use windowGenerator to generate datasets

--- dataengineering.py
+++ dataengineering.py
@@ -2,11 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+from WindowGen import WindowGenerator
 
 FILENAME = 'data/apple'
 
 def plot(df):
     plot_cols = ['open', 'change', 'volume']
+    df.pop('label')
     date_time = pd.to_datetime(df.pop('date'), format='%Y-%m-%d')
     plot_features = df[plot_cols]
     plot_features.index = date_time
@@ -31,6 +33,11 @@ def main():
     test_df = df[int(n*0.9):]
 
     num_features = df.shape[1]
+
+    w2 = WindowGenerator(input_width=6, label_width=1, shift=1,
+                         train_df=train_df, val_df=val_df, test_df=test_df,
+                            label_columns=['open'])
+    w2.plot()
 
 if __name__ == '__main__':
     main()

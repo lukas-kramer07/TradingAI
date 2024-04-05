f500dc84800d555c0d717d43df55fefdc593c95a
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-05 11:42:35 +0200

    feat: add plot method

--- WindowGen.py
+++ WindowGen.py
@@ -3,6 +3,8 @@ WindowGenerator adapted from https://www.tensorflow.org/tutorials/structured_dat
 '''
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
 
 class WindowGenerator():
   def __init__(self, input_width, label_width, shift,
@@ -55,3 +57,37 @@ class WindowGenerator():
     labels.set_shape([None, self.label_width, None])
 
     return inputs, labels
+  
+  def plot(self, model=None, plot_col='T (degC)', max_subplots=3):
+    inputs, labels = self.example
+    plt.figure(figsize=(12, 8))
+    plot_col_index = self.column_indices[plot_col]
+    max_n = min(max_subplots, len(inputs))
+    for n in range(max_n):
+        plt.subplot(max_n, 1, n+1)
+        plt.ylabel(f'{plot_col} [normed]')
+        plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+                label='Inputs', marker='.', zorder=-10)
+
+        if self.label_columns:
+            label_col_index = self.label_columns_indices.get(plot_col, None)
+        else:
+            label_col_index = plot_col_index
+
+        if label_col_index is None:
+            continue
+
+        plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                    edgecolors='k', label='Labels', c='#2ca02c', s=64)
+        if model is not None:
+            predictions = model(inputs)
+            plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                    marker='X', edgecolors='k', label='Predictions',
+                    c='#ff7f0e', s=64)
+
+        if n == 0:
+            plt.legend()
+
+    plt.xlabel('Time [h]')
+
+    

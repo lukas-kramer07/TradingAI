3ea6e96585c223aee94a51aaee15f7d7440e7396
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-06 12:31:21 +0200

    feat: create single step window

--- single_step_models.py
+++ single_step_models.py
@@ -1,4 +1,5 @@
 ## A number of single step models, prediciting one day into the future based on the last day of data
+## They predict the closing_value of the day
 
 #imports
 import pandas as pd
@@ -14,8 +15,11 @@ from dataengineering import return_data
 def main():
     #get data
     train_df, val_df, test_df, column_indices, num_features = return_data()
+    single_step_window = WindowGenerator(
+        train_df=train_df, val_df=val_df, test_df=test_df,
+        input_width=1, label_width=1, shift=1,
+        label_columns=['close'])
     
-
 
 if __name__ == '__main__':
     main()

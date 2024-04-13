1c30ac2e9e51837adf96dbd8d9deee9a13b98061
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-13 11:05:17 +0200

    feat: standardize df

--- utils/dataengineering.py
+++ utils/dataengineering.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+import seaborn as sns
 
 FILENAME = 'data/apple'
 
@@ -32,6 +33,21 @@ def return_data(filename = FILENAME):
     test_df = df[int(n*0.9):]
 
     num_features = df.shape[1]
+
+    # Standardize
+    train_mean = train_df.mean()
+    train_std = train_df.std()
+
+    train_df = (train_df - train_mean) / train_std
+    val_df = (val_df - train_mean) / train_std
+    test_df = (test_df - train_mean) / train_std
+
+    # plot 
+    df_std = (df - train_mean) / train_std
+    df_std = df_std.melt(var_name='Column', value_name='Normalized')
+    plt.figure(figsize=(12, 6))
+    ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+    _ = ax.set_xticklabels(df.keys(), rotation=90)
 
     return train_df, val_df, test_df, column_indices, num_features
 

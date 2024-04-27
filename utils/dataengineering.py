765af46e3194bb2c1964583466c33e3d3ea56678
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-27 17:34:31 +0200

    feat: add option to standardize over single datasets or over whole dataset

--- utils/dataengineering.py
+++ utils/dataengineering.py
@@ -5,6 +5,7 @@ import numpy as np
 import seaborn as sns
 import os
 
+STANDARDOVERSINGLEDATA = True
 FILENAME = 'data/GenElectric'
 
 def plot(df, date_time):
@@ -44,29 +45,39 @@ def iterate_files(folder_path):
             yield filename
 def standardize(df, mean, std):
     return (df-mean)/std
-def concat_data(folder):
-    # concat all data
-    standard, train_df, val_df, test_df= None,[],[],[]
-    for filename in iterate_files(folder):
-        train_df1, val_df1, test_df1, column_indices, num_features = return_data(filename=f'{folder}/{filename}')
-        standard = pd.concat([standard, train_df1])
-        train_df.append(train_df1)
-        val_df.append(val_df1)
-        test_df.append(test_df1)
-    
-    #standardize
-    mean = standard.mean()
-    std = standard.std()
-    train_df = [standardize(df, mean,std) for df in train_df]
-    val_df = [standardize(df, mean,std) for df in val_df]
-    test_df = [standardize(df, mean,std) for df in test_df]
-    """# plot 
-    df_std = (val_df - train_mean) / train_std
-    df_std = df_std.melt(var_name='Column', value_name='Normalized')
-    plt.figure(figsize=(12, 6))
-    ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
-    _ = ax.set_xticklabels(val_df.keys(), rotation=90)     
-    plt.show()"""
+def concat_data(folder, standard = STANDARDOVERSINGLEDATA):
+    if standard:
+        train_df, val_df, test_df= None,[],[],[]
+        for filename in iterate_files(folder):
+            train_df1, val_df1, test_df1, column_indices, num_features = return_data(filename=f'{folder}/{filename}')
+            mean = train_df1.mean()
+            std = train_df1.std()
+            train_df.append(standardize(train_df1, mean, std))
+            val_df.append(standardize(val_df1, mean, std))
+            test_df.append(standardize(test_df1, mean, std))
+    else:
+        # concat all data
+        standard, train_df, val_df, test_df= None,[],[],[]
+        for filename in iterate_files(folder):
+            train_df1, val_df1, test_df1, column_indices, num_features = return_data(filename=f'{folder}/{filename}')
+            standard = pd.concat([standard, train_df1])
+            train_df.append(train_df1)
+            val_df.append(val_df1)
+            test_df.append(test_df1)
+        
+        #standardize over whole dataset
+        mean = standard.mean()
+        std = standard.std()
+        train_df = [standardize(df, mean,std) for df in train_df]
+        val_df = [standardize(df, mean,std) for df in val_df]
+        test_df = [standardize(df, mean,std) for df in test_df]
+        """# plot 
+        df_std = (val_df - train_mean) / train_std
+        df_std = df_std.melt(var_name='Column', value_name='Normalized')
+        plt.figure(figsize=(12, 6))
+        ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+        _ = ax.set_xticklabels(val_df.keys(), rotation=90)     
+        plt.show()"""
 
     return train_df, val_df, test_df, column_indices, num_features
 def main():

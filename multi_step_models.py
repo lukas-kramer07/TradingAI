835ee5d4d78d74743a23ce765a529f93fd8108bd
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-04-29 11:55:07 +0200

    feat: add call function

--- multi_step_models.py
+++ multi_step_models.py
@@ -62,6 +62,32 @@ class FeedBack(tf.keras.Model):
       # predictions.shape => (batch, features)
       prediction = self.dense(x)
       return prediction, state
+   def call(self, inputs, training=None):
+      # Use a TensorArray to capture dynamically unrolled outputs.
+      predictions = []
+      # Initialize the LSTM state.
+      prediction, state = self.warmup(inputs)
+
+      # Insert the first prediction.
+      predictions.append(prediction)
+
+      # Run the rest of the prediction steps.
+      for n in range(1, self.out_steps):
+         # Use the last prediction as input.
+         x = prediction
+         # Execute one lstm step.
+         x, state = self.lstm_cell(x, states=state,
+                                    training=training)
+         # Convert the lstm output to a prediction.
+         prediction = self.dense(x)
+         # Add the prediction to the output.
+         predictions.append(prediction)
+
+      # predictions.shape => (time, batch, features)
+      predictions = tf.stack(predictions)
+      # predictions.shape => (batch, time, features)
+      predictions = tf.transpose(predictions, [1, 0, 2])
+      return predictions
 
 
 def test(model, window, name):

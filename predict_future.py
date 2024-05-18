e579fc4c5e8bf09bb83449f9beeeaf45a4109ee0
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-18 09:28:44 +0200

    feat: get data

--- predict_future.py
+++ predict_future.py
@@ -1,1 +1,15 @@
  # Script to predict a possilbe movement using the single_step/multi_step models 150 days into the future
+from utils import getdata
+from datetime import datetime
+
+def main(len = 356):
+    end = datetime.now()
+    start = end.replace(year=end.year-1, )#day=end.day+1)
+    end = end.strftime('%Y-%m-%dT00')
+    start = start.strftime('%Y-%m-%dT00')
+    symbol = input('Symbol to predict: ')
+    data = getdata(start, end, data_name=symbol)
+    print(data)
+
+if __name__ == '__main__':
+    main()

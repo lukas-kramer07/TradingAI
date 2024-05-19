4feae4258deeff0c0fa7c94a336ee3e57a3fb615
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-19 20:11:41 +0200

    account for holidays and such

--- predict_future.py
+++ predict_future.py
@@ -1,14 +1,15 @@
  # Script to predict a possilbe movement using the single_step/multi_step models 150 days into the future
 from utils import getdata
 from datetime import datetime, timedelta
+import pandas as pd
 
 def main(len = 356):
     end = datetime.now()
-    start = end - timedelta(hours=len)
+    start = end - timedelta(hours=len*5)
     end = end.strftime('%Y-%m-%dT00')
     start = start.strftime('%Y-%m-%dT00')
     symbol = input('Symbol to predict: ')
-    data = getdata(start, end, data_name=symbol)
+    data = getdata(start, end, data_name=symbol).tail(len)
     print(data)
 
 if __name__ == '__main__':

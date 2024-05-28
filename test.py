333fec6ffaa11bfc015aebc5c66493283c0371e4
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-28 10:24:22 +0200

    train: multi_step models

---
+++ test.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+
+gpus = tf.config.list_physical_devices("GPU")
+print(gpus)
+if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+        logical_gpus = tf.config.list_logical_devices("GPU")
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
+gpus

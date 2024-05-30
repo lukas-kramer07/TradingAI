1dcd09e0e5669d60df53b8b4a4a8258268824ae8
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-30 13:23:20 +0200

    feat: get data, define window

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -5,19 +5,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+from zmq import OUT_BATCH_SIZE
 from single_step_models import ResidualWrapper
 from utils import WindowGenerator
 from utils import concat_data, compile_and_fit, plot
-from multi_step_models import HISTORY, PERFORMANCE, RETRAIN, VAL_PERFORMANCE, LastStepBaseline
+from multi_step_models import HISTORY, OUT_STEPS, PERFORMANCE, RETRAIN, VAL_PERFORMANCE, LastStepBaseline
 import os
 
 RETRAIN = True
 VAL_PERFORMANCE = {}
 PERFORMANCE = {}
 HISTORY={}
-
+OUT_STEPS=500
+IN_STEPS=500
 def main():
-    pass
+    #get data
+    train_df, val_df, test_df, column_indices, num_features = concat_data('data', standard=False)
+    # define windows
+    multi_window = WindowGenerator(train_df=train_df, val_df = val_df, test_df=test_df,
+                                    input_width=IN_STEPS,
+                                    label_width=OUT_STEPS,
+                                    shift=1, label_columns=['c'])
 
 def test(model, window, name):
     VAL_PERFORMANCE[name] = model.evaluate(window.val, return_dict=True)

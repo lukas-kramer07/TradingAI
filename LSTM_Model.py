d4b3e285f79c1318700a4d2220e14e5689cf3e65
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-30 13:21:25 +0200

    feat: add train_and_test

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -8,18 +8,28 @@ import numpy as np
 from single_step_models import ResidualWrapper
 from utils import WindowGenerator
 from utils import concat_data, compile_and_fit, plot
-from multi_step_models import PERFORMANCE, VAL_PERFORMANCE, LastStepBaseline
+from multi_step_models import HISTORY, PERFORMANCE, RETRAIN, VAL_PERFORMANCE, LastStepBaseline
 import os
 
+RETRAIN = True
 VAL_PERFORMANCE = {}
 PERFORMANCE = {}
+HISTORY={}
 
 def main():
     pass
+
 def test(model, window, name):
     VAL_PERFORMANCE[name] = model.evaluate(window.val, return_dict=True)
     PERFORMANCE[name] = model.evaluate(window.test, verbose=0, return_dict=True)
 
+def train_and_test(model, window, model_name, patience=3 ,retrain = RETRAIN):
+  if model_name not in os.listdir('Training/Models/multi') or retrain:
+    HISTORY[model_name] = compile_and_fit(model, window, patience)
+    model.save(f'Training/Models/multi/{model_name}')
+  else:
+     model = tf.keras.models.load_model(f'Training/Models/multi/{model_name}')
+  test(model,window,model_name)
 
 
 if __name__ == '__main__':

4a2577eb02feb7ff92c6b6f0756eae86ce5e837a
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-30 13:18:09 +0200

    feat: add test

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -8,11 +8,18 @@ import numpy as np
 from single_step_models import ResidualWrapper
 from utils import WindowGenerator
 from utils import concat_data, compile_and_fit, plot
-from multi_step_models import LastStepBaseline
+from multi_step_models import PERFORMANCE, VAL_PERFORMANCE, LastStepBaseline
 import os
+
+VAL_PERFORMANCE = {}
+PERFORMANCE = {}
 
 def main():
     pass
+def test(model, window, name):
+    VAL_PERFORMANCE[name] = model.evaluate(window.val, return_dict=True)
+    PERFORMANCE[name] = model.evaluate(window.test, verbose=0, return_dict=True)
+
 
 
 if __name__ == '__main__':

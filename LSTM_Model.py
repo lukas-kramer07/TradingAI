94055b951c14af68d8482f43e1b34049059575ae
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-30 13:17:16 +0200

    add structure

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -8,4 +8,12 @@ import numpy as np
 from single_step_models import ResidualWrapper
 from utils import WindowGenerator
 from utils import concat_data, compile_and_fit, plot
+from multi_step_models import LastStepBaseline
 import os
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

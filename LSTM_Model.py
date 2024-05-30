fd69644b2507398c714058eccd74efa0fca89805
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-30 13:16:06 +0200

    init

---
+++ LSTM_Model.py
@@ -0,0 +1,11 @@
+# Advanced LSTM model specialized on prediciting stock prices
+
+#imports
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from single_step_models import ResidualWrapper
+from utils import WindowGenerator
+from utils import concat_data, compile_and_fit, plot
+import os

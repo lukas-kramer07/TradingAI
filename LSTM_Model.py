ea87af989481cfefcc37f0a04bc175c871813c4f
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-05-31 19:01:50 +0200

    feat: add LSTM model

--- LSTM_Model.py
+++ LSTM_Model.py
@@ -16,8 +16,8 @@ RETRAIN = True
 VAL_PERFORMANCE = {}
 PERFORMANCE = {}
 HISTORY={}
-OUT_STEPS=500
-IN_STEPS=500
+OUT_STEPS=1000
+IN_STEPS=1000
 def main():
     #get data
     train_df, val_df, test_df, column_indices, num_features = concat_data('data', standard=False)
@@ -26,6 +26,21 @@ def main():
                                     input_width=IN_STEPS,
                                     label_width=OUT_STEPS,
                                     shift=1, label_columns=['c'])
+    last_baseline = LastStepBaseline(label_index=column_indices['c'])
+    train_and_test(last_baseline, multi_window, 'lastBaseline')
+
+    LSTM_model = tf.keras.Sequential([
+      tf.keras.layers.LSTM(256, return_sequences=True, input_shape=(1000, 1)),
+      tf.keras.layers.Dropout(0.2),
+      tf.keras.layers.LSTM(128, return_sequences=True),
+      tf.keras.layers.Dropout(0.2),
+      tf.keras.layers.LSTM(64),
+      tf.keras.layers.Dense(OUT_STEPS * 1),
+      tf.keras.layers.Reshape([OUT_STEPS, 1])
+    ])
+    train_and_test(LSTM_model, multi_window, 'improved_LSTM')
+
+    plot(VAL_PERFORMANCE, PERFORMANCE)
 
 def test(model, window, name):
     VAL_PERFORMANCE[name] = model.evaluate(window.val, return_dict=True)

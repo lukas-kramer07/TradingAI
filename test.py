e7fef666db8f549e1cc6ce768023d581e9be2e2d
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-07-11 15:40:24 +0200

    feat: add Baseline model

--- test.py
+++ test.py
@@ -1,18 +1,65 @@
+# Advanced LSTM model specialized on prediciting stock prices
+
+#imports
+import pandas as pd
+import matplotlib.pyplot as plt
 import tensorflow as tf
-
 import numpy as np
+from single_step_models import ResidualWrapper
+from utils import WindowGenerator
+from utils import concat_data, plot
+from multi_step_models import LastStepBaseline
 import os
-import time
 
-gpus = tf.config.list_physical_devices("GPU")
-print(gpus)
-if gpus:
-    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
-    try:
-        tf.config.experimental.set_memory_growth(gpus[0], True)
-        logical_gpus = tf.config.list_logical_devices("GPU")
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-        # Virtual devices must be set before GPUs have been initialized
-        print(e)
-gpus
+RETRAIN = True 
+VAL_PERFORMANCE = {}
+PERFORMANCE = {}
+HISTORY = {}
+IN_STEPS=1000
+
+class Baseline(tf.keras.Model):
+  def __init__(self):
+    super().__init__()
+
+  def call(self, inputs):
+    return 0
+
+
+def main():
+    #get data
+    train_df, val_df, test_df, column_indices, num_features = concat_data('data', standard=False)
+    # define windows
+    multi_window = WindowGenerator(train_df=train_df, val_df = val_df, test_df=test_df,
+                                    input_width=IN_STEPS,
+                                    shift=1, label_columns=['c'])
+    print(multi_window.example)
+
+    # Training
+    print('Baseline')
+    model = Baseline()
+
+def test(model, window, name):
+    VAL_PERFORMANCE[name] = model.evaluate(window.val, return_dict=True)
+    PERFORMANCE[name] = model.evaluate(window.test, verbose=0, return_dict=True)
+
+def train_and_test(model, window, model_name, patience=15 ,retrain = RETRAIN):
+  if model_name not in os.listdir('Training/Models/multi') or retrain:
+    HISTORY[model_name] = compile_and_fit(model, window, patience)
+    model.save(f'Training/Models/multi/{model_name}')
+  else:
+     model = tf.keras.models.load_model(f'Training/Models/multi/{model_name}')
+  test(model,window,model_name)
+
+def compile_and_fit(model, window, patience=15, epochs=50):
+  early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                    patience=patience,
+                                                    mode='min')
+
+  model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001, clipnorm=1.0),
+                loss='mean_squared_error',
+                metrics=[tf.keras.metrics.MeanAbsoluteError(), tf.keras.metrics.MeanAbsolutePercentageError(), tf.keras.metrics.Accuracy()])
+
+  history = model.fit(window.train, epochs=epochs,
+                      validation_data=window.val,
+                      callbacks=[early_stopping])
+  return history

2c56f4677e8f6864a76dfd1a0793cf211c5bb9c8
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-07-19 19:41:28 +0200

    feat: add deep model

--- test.py
+++ test.py
@@ -66,7 +66,15 @@ def main():
       ])
     train_and_test(linear_model, window, 'Linear')
 
-    
+    print('deep model')
+    deep_model = keras.Sequential([
+       keras.layers.Flatten(),
+       keras.layers.Dense(64, activation='relu'),
+       keras.layers.Dropout(0.2),
+       keras.layers.Dense(64, activation='relu'),
+       keras.layers.Dense(5, activation='softmax')
+    ])
+    train_and_test(deep_model, window, 'Deep')
     print(tf.argmax(linear_model(window.example), axis=-1), window.example[1])
 
 def test(model, window, name):

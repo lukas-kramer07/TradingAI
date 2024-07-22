f08a70d4dd8a3c0495e419396f681cfdbc59b5fd
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-07-22 12:27:30 +0200

    train: add results

--- models.py
+++ models.py
@@ -96,7 +96,7 @@ def test(model, window, name):
     VAL_PERFORMANCE[name] = model.evaluate(window.val, return_dict=True)
     PERFORMANCE[name] = model.evaluate(window.test, verbose=0, return_dict=True)
 
-def train_and_test(model, window, model_name, patience=10 ,retrain = RETRAIN, epochs=30):
+def train_and_test(model, window, model_name, patience=5 ,retrain = RETRAIN, epochs=30):
   if model_name not in os.listdir('Training/Models') or retrain:
     HISTORY[model_name] = compile_and_fit(model, window, patience, epochs=epochs)
     model.save(f'Training/Models/{model_name}')

2f00f37c65ec2d7cab97ff77bc49e342dfafe02b
lukas-kramer07 <lukas.kramer7@gmail.com>
2024-08-01 22:47:08 +0200

    feat: add prediction

--- plot_pred.py
+++ plot_pred.py
@@ -8,36 +8,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main(len = 1000):
+
+    #get data specified by user
     end = datetime.now()
     start = end - timedelta(hours=len*5)
     end = end.strftime('%Y-%m-%dT00')
     start = start.strftime('%Y-%m-%dT00')
     symbol = input('Symbol to predict: ')
-    data = getdata(start, end, data_name=symbol).tail(len)
+    try:
+        data = getdata(start, end, data_name=symbol).tail(len)
+    except:
+        raise Exception('Wrong Data Name or too little Data available')
+
+
     date_time = pd.to_datetime(data.pop('t'), format='%Y-%m-%dT%H:%M:%SZ')
-    mean,mean0 = data.mean(), data['c'].mean()
-    std,std0 = data.std(), data['c'].std()
+    mean = data.mean()
+    std = data.std()
     standard_data = standardize(data,mean,std)
 
     tensor = tf.expand_dims(tf.convert_to_tensor(data.values), 0)
     
-    model = tf.keras.models.load_model('Training/Models/multi/multi_dense')
+    model = tf.keras.models.load_model('Training/Models/LSTM')
     prediction = model.predict(tensor)
-    print(prediction.shape)
-    #plot
-    # Inverse the normalization
-    original_scale_pred = prediction.flatten()#((prediction * std0) + mean0).flatten()
-    original_scale_input = tf.convert_to_tensor(data['c'].values)#tf.convert_to_tensor(((data['c']*std0)+mean0).values)
-    full_pred = tf.concat([original_scale_input, original_scale_pred],0)
-    # Convert the tensor to a 1D array for plotting (if necessary)
-    pred_flattened = full_pred
+    print(prediction.shape, prediction)
 
-    # Plotting the values
-    plt.plot(pred_flattened)
-    plt.title(f"Plot of {symbol}'s predicted stock")
-    plt.xlabel("future days")
-    plt.ylabel("Value")
-    plt.show()
 
 if __name__ == '__main__':
     main()
